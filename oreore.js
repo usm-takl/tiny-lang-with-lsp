4083bb229294877984a2aa2c18b36f14f96db578
usm-takl <usm.takl@gmail.com>
2020-12-22 12:36:01 +0900

    add didOpen/didChange

--- oreore.js
+++ oreore.js
@@ -73,11 +73,34 @@ const requestTable = {};
 const notificationTable = {};
 
 requestTable["initialize"] = (msg) => {
-    sendMessage({ jsonrpc: "2.0", id: msg.id, result: { capabilities: {} } });
+    const capabilities = {
+        textDocumentSync: 1,
+    };
+
+    sendMessage({ jsonrpc: "2.0", id: msg.id, result: { capabilities } });
 }
 
 notificationTable["initialized"] = (msg) => {
     logMessage("initialized!");
+}
+
+function compile(uri, src) {
+    logMessage(uri + ":" + src);
+    // TODO: implement
+}
+
+notificationTable["textDocument/didOpen"] = (msg) => {
+    const uri = msg.params.textDocument.uri;
+    const text = msg.params.textDocument.text;
+    compile(uri, text);
+}
+
+notificationTable["textDocument/didChange"] = (msg) => {
+    if (msg.params.contentChanges.length !== 0) {
+        const uri = msg.params.textDocument.uri;
+        const text = msg.params.contentChanges[msg.params.contentChanges.length - 1].text;
+        compile(uri, text);
+    }
 }
 
 function dispatch(msg) {
